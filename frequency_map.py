be3bef3dfc687844e5dcc1c1c2ffc1352b44368b
Shivansh7102003 <shivansh.2245729@mygyanvihar.com>
2025-11-12 12:37:49 +0530

    feat(frequency_map): add frequency counting implementation using dictionaries

---
+++ frequency_map.py
@@ -0,0 +1,21 @@
+# to find the frequncy of number present in the list and store them in dictionary in form of key value pair 
+num = [5,6,7,8,4,4,6,4,6,48,2,4,2,6,7,2,4,5,2]
+frequency_map = {}
+
+for i in range(0 , len(num)):
+    if num[i] not in frequency_map:
+        frequency_map[num[i]] = 1
+    else :
+        frequency_map[num[i]] = frequency_map[num[i]]+1
+        
+print(frequency_map)
+
+
+# method 2 using hasmaps 
+
+hash_maps  = {}
+
+for i in range(0,len(num)):
+    hash_maps[num[i]]= hash_maps.get(num[0] , 0) +1
+    
+print(hash_maps)
